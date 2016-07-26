53328d03ebc955059e041d4a0250aae52b7b14e2
Tatiana Redchits <7_tatka_7@mail.ru>
2016-07-26 22:19:51 +0300

    Changed rounding

--- Offline_JS/Math function/script.js
+++ Offline_JS/Math function/script.js
@@ -2,25 +2,25 @@
 
 function calculateKineticEnergy(m, v) {
     if (m < 0 || v < 0 || m == undefined || v == undefined || +m != m || +v != v) {
-        return ('invalid data');
+        return NaN;
     }
-    return Math.round(m * Math.pow(v, 2) / 2 * 100 ) / 100;
+    return m * Math.pow(v, 2) / 2;
 }
 
-console.log('Kinetic Energy: ' + calculateKineticEnergy(1, 1.39));
-console.log('Kinetic Energy: ' + calculateKineticEnergy(20, 1.96));
-console.log('Kinetic Energy: ' + calculateKineticEnergy(0.75));
-console.log('Kinetic Energy: ' + calculateKineticEnergy(-6, 1.1));
-console.log('Kinetic Energy: ' + calculateKineticEnergy(54, 3.9));
-console.log('Kinetic Energy: ' + calculateKineticEnergy(54, '3.9'));
-console.log('Kinetic Energy: ' + calculateKineticEnergy(54, 'sg'));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(1, 1.39).toFixed(2));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(20, 1.96).toFixed(2));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(0.75).toFixed(2));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(-6, 1.1).toFixed(2));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(54, 3.9).toFixed(2));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(54, '3.9').toFixed(2));
+console.log('Kinetic Energy: ' + calculateKineticEnergy(54, 'sg').toFixed(2));
 console.log('');
 
 /*******   Calculate Space Of Rectangle   *******/ 
 
 function calculateSpaceOfRectangle(a, b) {
     if (a < 0 || b < 0 || a == undefined || b == undefined || +a != a || +b != b) {
-        return ('invalid data');
+        return NaN;
     }
     return a * b;
 }
@@ -36,49 +36,49 @@ console.log('');
 
 function calculateSpaceOfCircle(r) {
     if (r < 0 || r == undefined || +r != r) {
-        return ('invalid data');
+        return NaN;
     }
-    return Math.round(Math.PI * Math.pow(r, 2) * 100) / 100;
+    return Math.PI * Math.pow(r, 2);
 }
 
-console.log('Space Of Circle: ' + calculateSpaceOfCircle(16));
-console.log('Space Of Circle: ' + calculateSpaceOfCircle(0));
-console.log('Space Of Circle: ' + calculateSpaceOfCircle(3.67));
-console.log('Space Of Circle: ' + calculateSpaceOfCircle('3.67'));
-console.log('Space Of Circle: ' + calculateSpaceOfCircle(-5));
-console.log('Space Of Circle: ' + calculateSpaceOfCircle('dfas'));
+console.log('Space Of Circle: ' + calculateSpaceOfCircle(16).toFixed(2));
+console.log('Space Of Circle: ' + calculateSpaceOfCircle(0).toFixed(2));
+console.log('Space Of Circle: ' + calculateSpaceOfCircle(3.67).toFixed(2));
+console.log('Space Of Circle: ' + calculateSpaceOfCircle('3.67').toFixed(2));
+console.log('Space Of Circle: ' + calculateSpaceOfCircle(-5).toFixed(2));
+console.log('Space Of Circle: ' + calculateSpaceOfCircle('dfas').toFixed(2));
 console.log('');
 
 /*******   Calculate Space Of Ellipse   *******/ 
 
 function calculateSpaceOfEllipse(r1, r2) {
     if (r1 < 0 || r2 < 0 || r1 == undefined || r2 == undefined || +r1 != r1 || +r2 != r2) {
-        return ('invalid data');
+        return NaN;
     }
-    return Math.round(Math.PI * r1 * r2 * 100) / 100; 
+    return Math.PI * r1 * r2; 
 }
 
-console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(6, 8));
-console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(19));
-console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(15.85, 45.6));
-console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse('rtyhmj'));
-console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(-84));
+console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(6, 8).toFixed(2));
+console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(19).toFixed(2));
+console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(15.85, 45.6).toFixed(2));
+console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse('rtyhmj').toFixed(2));
+console.log('Space Of Ellipse: ' + calculateSpaceOfEllipse(-84).toFixed(2));
 console.log('');
 
 /*******   Calculate Distance Between Two Points   *******/ 
 
 function calculateDistanceBetweenTwoPoints(x1, y1, x2, y2) {
     if (x1 == undefined || y1 == undefined || x2 == undefined || y2 == undefined || +x1 != x1 || +y1 != y1 || +x2 != x2 || +y2 != y2) {
-        return ('invalid data');
+        return NaN;
     }
-    return Math.round(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) * 100) / 100;
+    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 }
 
-console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints(-1, 3, 6, 2));
-console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints(-1.85, 3.75, 0, -6));
-console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints('-1.85', '3.75', 0, '-6'));
-console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints('-1.85', 'oif', 0, '-6'));
-console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints(3, 6, 2));
+console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints(-1, 3, 6, 2).toFixed(2));
+console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints(-1.85, 3.75, 0, -6).toFixed(2));
+console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints('-1.85', '3.75', 0, '-6').toFixed(2));
+console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints('-1.85', 'oif', 0, '-6').toFixed(2));
+console.log('Distance Between Two Points: ' + calculateDistanceBetweenTwoPoints(3, 6, 2).toFixed(2));
 console.log('');
 
 /*******   Calculate Profit   *******/ 
@@ -87,7 +87,7 @@ function calculateProfit(arr) {
     var profit = 0;
     for (var i = 0; i < arr.length; ++i) {
         if (isNaN(arr[i])) {
-            return ('invalid data');
+            return NaN;
         }
         profit += Number(arr[i]);
     }
@@ -110,7 +110,7 @@ function calculateFallingTime(h) {
     if (h == 0) {
         return ('Climb higher!');
     }
-    return Math.round(Math.sqrt(2 * h / G) * 10) / 10;
+    return Math.sqrt(2 * h / G);
 }
 
 console.log('Falling Time: ' + calculateFallingTime(5000));
